2f98d09a9ff3ad495709dae8094c7001444ef8cd
Ryo Shirotsuki <whiter462@gmail.com>
2017-10-24 09:34:44 +0900

    Update texlive.rb

--- Formula/texlive.rb
+++ Formula/texlive.rb
@@ -3,13 +3,13 @@ class Texlive < Formula
   homepage "https://www.tug.org/texlive/"
   url "http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz"
   version "20171022"
-  sha256 "925bceeb4100ff7248dd499f732901d135f4747598bb64c93a1bc51928f65a2b"
+  sha256 "f91af0dc3ab940b6c15a9ee1abe222fea84cca82e9738efc93cd9e8ac20271c8"
   # tag "linuxbrew"
 
   bottle do
     sha256 "f529174ec1714d92a515604133c6bcd9b75224d995f013e84203a0a0e73a47b6" => :x86_64_linux # glibc 2.19
   end
-
+  
   option "with-full", "install everything"
   option "with-medium", "install small + more packages and languages"
   option "with-small", "install basic + xetex, metapost, a few languages [default]"
